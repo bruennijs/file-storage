41aa08e48ba264025c65c9e443347110d658ca9a
Olli B <oliver.bruentje@gmx.de>
2016-02-26 15:39:50 +0100

    project.json try to integrate in dotnet core environment

--- file-storage.core/application/files/FilesService.cs
+++ file-storage.core/application/files/FilesService.cs
@@ -1,22 +1,24 @@
 using System;
 using System.IO;
-//using log4net;
+using log4net;
 
 namespace FileStorage.application.services
 {
+    
+    
     /// <summary>
     /// Application file service.
     /// </summary>
     public class FilesService {
-      //private static ILog Log = LogManager.GetLogger(typeof(FilesService));  
+      private static ILog Log = LogManager.GetLogger(typeof(FilesService));  
         
       public bool Create(string name) {
           
-          //Log.InfoFormat("FilesService.Create {0}", name);
+          Log.InfoFormat("FilesService.Create {0}", name);
           
           Console.WriteLine($"name is {name}");
           
-          var path = Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, "test.txt");
+          var path = Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location, "test.txt");         
           
           //JsonConvert.DeserializeObject<File
           
--- file-storage.core/domain/files/File.cs
+++ file-storage.core/domain/files/File.cs
@@ -2,5 +2,7 @@ namespace FileStorage.domain.files
 {
   public class File
   {    
+      // Gets or sets the file name
+      public string Name { get; set;}
   } 
 }
