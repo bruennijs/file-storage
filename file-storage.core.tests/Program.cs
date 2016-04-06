fb67f11cf5d9c1004dd3a75db12e2925402f633c
Olli B <oliver.bruentje@gmx.de>
2016-04-06 22:16:13 +0200

    running with dnx451 and mono dnx

---
+++ file-storage.core.tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+using NUnitLite;
+
+namespace Redis.test
+{
+  public class Program
+  {
+    private static ILog _log = log4net.LogManager.GetLogger(typeof(Program));
+
+    public static void Main(string[] args)
+    {
+      //Console.WriteLine(Assembly.GetExecutingAssembly().CodeBase);
+
+      log4net.Config.XmlConfigurator.Configure();
+      _log.Info("test starting...");
+
+      new AutoRun().Execute(args);
+    }
+  }
+}
--- file-storage.core/AutoFacModule.cs
+++ file-storage.core/AutoFacModule.cs
@@ -1,4 +1,4 @@
-using Autofac;
+﻿using Autofac;
 using filestorage.core.application;
 using FileStorage.domain.identity;
 
