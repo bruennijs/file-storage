e532ce00c5c4fc944de801de79ebc09ebdfd6500
Olli B <oliver.bruentje@gmx.de>
2016-04-07 23:29:03 +0200

    Integrated common.ddd

--- file-storage.core/application/files/FilesApplicationService.cs
+++ file-storage.core/application/files/FilesApplicationService.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using FileStorage.domain.files;
 using log4net;
+using common.ddd.Domain.Entity;
 
 namespace filestorage.core.application
 {
@@ -11,9 +12,12 @@ namespace filestorage.core.application
     /// </summary>
     public class FilesApplicationService : IFilesApplicationService {
       private static ILog Log = LogManager.GetLogger(typeof(FilesApplicationService));
-        
-      public bool Create(string name) {
-          
+
+      public bool Create(string name)
+      {
+
+        var file = new FileStorage.domain.files.File(new Id("4711"), "meineDatei.txt");
+
           Log.InfoFormat("FilesService.Create {0}", name);
           
           Console.WriteLine($"name is {name}");
--- file-storage.core/domain/files/File.cs
+++ file-storage.core/domain/files/File.cs
@@ -1,8 +1,18 @@
+using common.ddd.Domain.Entity;
+
 namespace FileStorage.domain.files
 {
   public class File
   {    
+      public Id Id { get; private set; }
+
       // Gets or sets the file name
-      public string Name { get; set;}
+      public string Name { get; private set;}
+
+    public File(Id id, string name)
+    {
+      Id = id;
+      Name = name;
+    }
   } 
 }
