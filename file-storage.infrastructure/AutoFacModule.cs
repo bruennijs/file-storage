78fa89e0375547b6ec8adfd9fe1166c1e2270027
Olli B <oliver.bruentje@gmx.de>
2016-04-18 23:12:19 +0200

    file-storage.infrastructure base work

---
+++ file-storage.infrastructure/AutoFacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using common.ddd.Infrastructure.Event;
+using common.ddd.Infrastructure.Event.Implementation;
+
+namespace common.ddd
+{
+  public class AutoFacModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<DomainEventBus>().As<IDomainEventBus>().SingleInstance();
+    }
+  }
+}
---
+++ file-storage.infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("common.ddd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("common.ddd")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("2EBC28CF-2D1A-46BB-A08E-AB592A00115B")]
+
